48c6daa8a67f84d48f3569f07c842b71238b418b
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-08-08 21:35:15 -0500

    Pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 require("dotenv").config();
+let connection;
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+}
 
 connection.connect((err) => {
   if (err) throw err;
